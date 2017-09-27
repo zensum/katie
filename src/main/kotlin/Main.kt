7e28adef61f7c945c7db8f446deaaaed19ccf850
Anton Österberg <anton.osterberg@gmail.com>
2017-09-27 17:57:25 +0200

    Some more progress :)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 package se.zensum.katie
 
+import franz.JobStatus
 import franz.WorkerBuilder
-import franz.asPipe
 import se.zensum.webhook.PayloadOuterClass
 import java.net.URL
 
@@ -13,21 +13,42 @@ fun main(args: Array<String>) {
     WorkerBuilder.ofByteArray
         .subscribedTo(routes.keys)
         .groupId("katie")
-        .running {
-            asPipe()
-                .map(::Payload)
+        .handlePiped {
+            val topic: String = it.value?.topic() ?: return@handlePiped JobStatus.PermanentFailure
+            val url: URL? = routes[topic]
+            it
+                .require("URL is not null for topic ($topic)") { url != null }
+                .map { Payload(it.value()) }
+                .advanceIf("Not in idempotence store") { false }
+                .execute("Send to $url") { send(url!!, it) }
+                .execute("Write in idempotence store") { false }
                 .end()
-            TODO("We can not read topic yet. We need to use a more recent version of Franz.")
         }.start()
 }
 
-private fun <T> WorkerBuilder<T>.subscribedTo(topics: Collection<String>): WorkerBuilder<T> {
-    val topicArray = topics.toTypedArray()
-    return merge(this, topicArray)
-}
-
-private tailrec fun <T> merge(workerBuilder: WorkerBuilder<T>, topics: Array<String>, i: Int = 0): WorkerBuilder<T> {
-    return merge(workerBuilder.subscribedTo(topics[i]), topics, i+1)
-}
-
 fun Payload(bytes: ByteArray): PayloadOuterClass.Payload = PayloadOuterClass.Payload.parseFrom(bytes)
+
+private suspend fun send(url: URL, p: PayloadOuterClass.Payload): Boolean {
+    khttp.async.request(
+        method = p.method.name,
+        headers = toMap(p.headers),
+        url = url.toString(),
+        params = TODO("p.parameters"),
+        data = p.body,
+        onError = TODO(),
+        onResponse = TODO()
+    )
+}
+
+fun toMap(headers: PayloadOuterClass.MultiMap): Map<String, String> {
+    return headers.pairList.asSequence()
+        .groupBy { it.key }
+        .map { it.key to merge(it.value) }
+        .toMap()
+}
+
+private fun merge(pairs: List<PayloadOuterClass.MultiMap.Pair>): String {
+    return pairs.asSequence()
+        .map { it.value }
+        .joinToString(separator = ", ")
+}
