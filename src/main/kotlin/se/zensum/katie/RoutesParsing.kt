648bb13957c00debfd95f9c1643b5d8122bd028a
Daniel <daniel.torbacka@gmail.com>
2018-01-31 15:36:31 +0100

    Added support for one more route field

--- src/main/kotlin/se/zensum/katie/RoutesParsing.kt
+++ src/main/kotlin/se/zensum/katie/RoutesParsing.kt
@@ -4,43 +4,44 @@ import com.moandjiezana.toml.Toml
 import java.io.File
 import java.net.URL
 
-private const val ROUTES_FILE ="/etc/config/routes"
+private const val ROUTES_FILE = "/etc/config/routes"
 
-fun readRoutes(routesFile: String = getEnv("ROUTES_FILE", ROUTES_FILE)): Map<String, URL> {
+data class Route(val topic: String, val url: URL, val repsonseTopic: String)
+
+fun readRoutes(routesFile: String = getEnv("ROUTES_FILE", ROUTES_FILE)):Map<String, Route> {
     val toml = readTomlFromFile(routesFile)
     return parseTomlConfig(toml)
 }
 
-private fun readTomlFromFile(routesFile: String): Toml
-{
+private fun readTomlFromFile(routesFile: String): Toml {
     val file: File = verifyFile(routesFile)
     return Toml().read(file)
 }
 
-fun parseTomlConfig(toml: Toml): Map<String, URL> {
+fun parseTomlConfig(toml: Toml):  Map<String, Route> {
     val routes = toml.getTables("routes")
     return routes.asSequence()
         .map { parseEntry(it) }
         .toMap()
 }
 
-fun parseEntry(routeConfig: Toml): Pair<String, URL> {
+fun parseEntry(routeConfig: Toml): Pair<String, Route> {
     val topic: String = readOrDie(routeConfig, "topic")
     val url: URL = URL(readOrDie(routeConfig, "url"))
-    return topic to url
+    val responseRoute: String = readOrDie(routeConfig, "responseTopic")
+    return Pair(topic, Route(topic, url, responseRoute))
 }
 
 private fun readOrDie(config: Toml, key: String): String {
     return config.getString(key) ?: throw IllegalArgumentException("Missing mandatory key \"$key\" in config")
 }
 
-fun verifyFile(filePath: String): File
-{
+fun verifyFile(filePath: String): File {
     return File(filePath).apply {
-        if(!exists()) {
+        if (!exists()) {
             throw IllegalArgumentException("File $this does not exist")
         }
-        if(isDirectory) {
+        if (isDirectory) {
             throw IllegalArgumentException("File $this is a directory")
         }
     }
